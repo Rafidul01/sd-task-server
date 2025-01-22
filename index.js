4d0e40b4a0b84298494f074a4e234c8202a705c3
Rafidul01 <rafidulhasan555@gmail.com>
2025-01-23 01:51:56 +0600

    report api created

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-require('dotenv').config();
-const port = process.env.PORT || 5000; 
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 // middleware
 const corsOptions = {
-    origin: "http://localhost:5173", 
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  };
-  app.use(cors(corsOptions));
-  
-  app.use(express.json());
+  origin: "http://localhost:5173",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+};
+app.use(cors(corsOptions));
 
+app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hjxwn6k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,7 +22,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -34,29 +32,37 @@ async function run() {
 
     const navCollection = client.db("sirajgonjDairy").collection("navLinks");
     const sellCollection = client.db("sirajgonjDairy").collection("sells");
-    const dairyCollection = client.db("sirajgonjDairy").collection("dairyCollections");
+    const dairyCollection = client
+      .db("sirajgonjDairy")
+      .collection("dairyCollections");
 
-    app.get('/navLinks', async (req, res) => {
-        const result = await navCollection.find().toArray();
+    app.get("/navLinks", async (req, res) => {
+      const result = await navCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/:dbclollection/:category", async (req, res) => {
+      const type = req.params.dbclollection;
+      const category = req.params.category;
+      const query = { category: category };
+      if (type === "collection") {
+        const result = await dairyCollection.find(query).toArray();
         res.send(result);
-    })
-
-    app.get('/:dbclollection/:category', async (req, res) => {
-        const type = req.params.dbclollection;
-        const category = req.params.category;
-        const query = {category: category}
-        if(type === 'collection'){
-            const result = await dairyCollection.find(query).toArray();
-            res.send(result);
-        }
-        else if(type === 'sells'){
-            const result = await sellCollection.find(query).toArray();
-            res.send(result);
-        }
-    })
+      } 
+      else if (type === "sells") {
+        const result = await sellCollection.find(query).toArray();
+        res.send(result);
+      }
+      else if (type === "report") {
+        const result = await sellCollection.find().toArray();
+        res.send(result);
+      }
+    });
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -64,11 +70,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 app.get("/", (req, res) => {
-    res.send("Hello from server");
+  res.send("Hello from server");
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
